32296eea44d077755252f1baaef88b5ea93c2f32
Jardilab <jefferson.ardila.bayona@gmail.com>
2022-11-08 03:40:19 -0500

    CRUD API: PRODUCTS (FBDB)  - USERS (BDB) - ORDERS (BDB)

--- back/routes/orders.js
+++ back/routes/orders.js
@@ -2,8 +2,8 @@ const express=require("express");
 const router=express.Router();
 const { newOrder, 
     getOrderById, 
-    myOrders, 
-    getAllOrders,
+    getMyOrders,
+    getAllOrders, 
     updateOrder,
     deleteOrder
 } = require("../controllers/ordersController");
@@ -12,10 +12,9 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder)
 router.route("/order/:id").get(isAuthenticatedUser, getOrderById)
-router.route("/orders/myOrders").get(isAuthenticatedUser, myOrders)
+router.route("/orders/historyOrders").get(isAuthenticatedUser, getMyOrders)
 
-router.route("/admin/viewAllOrders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders)
-router.route('/admin/order/:id').get(isAuthenticatedUser, authorizeRoles("admin"), getOrderDetails)
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders)
 router.route("/admin/updateOrder/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
 router.route("/admin/deleteOrder/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder)
 
